0003479d56d89d9a8d3f13911c219ea150566215
Gavin <gavin.undefined@gmail.com>
2018-10-04 14:23:16 +0800

    create time-line

---
+++ src/index.js
@@ -0,0 +1,27 @@
+Component({
+  properties: {
+    data: {
+      type: 'Array',
+      value: [
+        {
+          icon: '',
+          time: '07-08 18:00',
+          title: 'Gavin分享了产品',
+          desc: '低调而不失奢华，是上乘佳作。',
+        },
+        {
+          icon: '',
+          time: '07-08 18:00',
+          title: 'Gavin分享了产品',
+          desc: '低调而不失奢华，是上乘佳作。',
+        },
+        {
+          icon: '',
+          time: '07-08 18:00',
+          title: 'Gavin分享了产品',
+          desc: '低调而不失奢华，是上乘佳作。',
+        }
+      ],
+    },
+  },
+})
